ead11b5486ce641c1f43a54db91e33b4cfe04890
Кузнецов Никита Вениаминович <kunv@kbl.nsk.su>
2022-11-10 12:04:56 +0700

    adding comments

--- main.cpp
+++ main.cpp
@@ -4,12 +4,23 @@
 #include <tuple>
 #include <type_traits>
 
+/// <summary>
+/// Home work 04 - SFINAE
+/// </summary>
+
 template<class T>
 using remove_cr_t = typename std::remove_const_t<std::remove_reference_t<T>>;
 
 template <typename> struct is_tuple : std::false_type {};
 template <typename ...T> struct is_tuple<std::tuple<T...>> : std::true_type {};
 
+/// <summary>
+/// Iteration by tuple.
+/// </summary>
+/// <typeparam name="T">Tuple type.</typeparam>
+/// <typeparam name="U">Tuple element type.</typeparam>
+/// <param name="Is">Ellipsis (of index).</param>
+/// <returns>Vector of tuple elements.</returns>
 template <typename T, typename U, std::size_t... Is>
 std::vector<U> tuple_to_vector_impl(const T& tuple, std::index_sequence<Is...>) {
     std::vector<U> vector;
@@ -21,11 +32,23 @@ std::vector<U> tuple_to_vector_impl(const T& tuple, std::index_sequence<Is...>) 
     return vector;
 }
 
+/// <summary>
+/// Convert tuple to vector.
+/// </summary>
+/// <typeparam name="T">Tuple type.</typeparam>
+/// <typeparam name="U">Tuple element type.</typeparam>
+/// <param name="Size">Tuple size.</param>
+/// <returns>std::vector of tuple elements.</returns>
 template <typename T, typename U, std::size_t Size = std::tuple_size_v<T>>
 std::vector<U> tuple_to_vector(const T& tuple) {
     return tuple_to_vector_impl<T, U>(tuple, std::make_index_sequence<Size>{});
 }
 
+/// <summary>
+/// Function <c>print_ip</c>.
+/// Implementation 1.
+/// </summary>
+/// <typeparam name="T">Container (except for the string).</typeparam>
 template <typename T>
 std::enable_if_t<
             std::is_same_v<decltype(begin(std::declval<T>()), end(std::declval<T>()), void()), void> &&
@@ -41,12 +64,22 @@ print_ip(const T& value) {
     std::cout << std::endl;
 }
 
+/// <summary>
+/// Function <c>print_ip</c>.
+/// Implementation 2.
+/// </summary>
+/// <typeparam name="T">String.</typeparam>
 template <typename T>
 std::enable_if_t<std::is_same_v<std::string, T>, void>
 print_ip(const T& value) {
     std::cout << value << std::endl;
 }
 
+/// <summary>
+/// Function <c>print_ip</c>.
+/// Implementation 3.
+/// </summary>
+/// <typeparam name="T">Tuple.</typeparam>
 template <typename T>
 std::enable_if_t<is_tuple<T>{}, void>
 print_ip(const T& value) {
@@ -55,6 +88,11 @@ print_ip(const T& value) {
     print_ip(ip);
 }
 
+/// <summary>
+/// Function <c>print_ip</c>.
+/// Implementation 4.
+/// </summary>
+/// <typeparam name="T">Integer type.</typeparam>
 template <typename T>
 std::enable_if_t<std::is_integral_v<T>, void>
 print_ip(const T& value) {
